4be5ab2f918bddf89bba2086b68c25f733496177
Muhammad jawad <95683395+Muhammad-jawad01@users.noreply.github.com>
2025-07-08 12:17:09 +0500

    update admin side

---
+++ routes/admin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Admin\ContactController;
+// use App\Http\Controllers\Admin\UserController;
+
+
+
+Route::group(
+    ['middleware' => ['auth', 'admin'], 'prefix' => 'admin', 'as' => 'admin.'],
+    function () {
+        // Route::get('/', function () {
+        //     return view('admin.index');
+        // })->name('dashboard');
+
+        Route::resource('users', UserController::class);
+        // Route::get('/', [UserController::class, 'index'])->name('users.index');
+        //users. index
+        //users.create
+        //users.store
+        //users.show
+        //users.edit
+        //users.update
+        //users.destroy
+        Route::resource('contacts', ContactController::class);
+    }
+);
